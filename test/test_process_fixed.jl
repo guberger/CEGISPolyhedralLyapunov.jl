17fc2cb26b10d72ac5c1f9a13990b53ce3fdf19f
guberger@PC-1r0-013 <guillaume.berger@uclouvain.be>
2022-02-11 17:21:24 -0700

    Add test process fixed

--- test/test_process_fixed.jl
+++ test/test_process_fixed.jl
@@ -25,30 +25,50 @@ solver = optimizer_with_attributes(HiGHS.Optimizer, "output_flag"=>false)
 ## Parameters
 ϵ = 1e-1
 tol = eps(1.0)
-δ_min = 1e-1
 D = 2
 M = 4
 
 ## Tests
 domain = zeros(1, D)
+fields = [[1.0 0; 0.0 1.0]]
+sys = CPL.LinearSystem(domain, fields)
+systems = (sys,)
+
+nodes_init = CPL.Node[]
+
+δ_min = 0.45
+coeffs, nodes, obj_max, flag =
+    CPL.process_PLF_fixed(M, D, systems, nodes_init,
+                          ϵ, tol, δ_min, solver,
+                          iter_max=Inf,
+                          output_period=100,
+                          learner_output=false)
+
+@testset "process_PLF_fixed: infeasible" begin
+    @test flag
+    @test obj_max > tol
+end
+
 fields = [[-101.1 99; 101 -99.1], [-101.1 -99; -101 -99.1]]
 sys = CPL.LinearSystem(domain, fields)
 systems = (sys,)
 
-# flows_init = map()
-# witness1 = CPL.Witness(flow1, 1)
-# witness2 = CPL.Witness(flow2, 2)
-# node1 = CPL.Node(witness1, 1)
-# node2 = CPL.Node(witness1, 2)
-# node3 = CPL.Node(witness2, 2)
-# nodes_init = (node1, node2, node3)
 nodes_init = CPL.Node[]
 
+δ_min = 0.25
 coeffs, nodes, obj_max, flag =
     CPL.process_PLF_fixed(M, D, systems, nodes_init,
                           ϵ, tol, δ_min, solver,
-                          iter_max=100000,
+                          iter_max=1000,
                           output_period=100,
                           learner_output=false)
+
+@testset "process_PLF_fixed: feasible" begin
+    @test flag
+    @test abs(obj_max + 0.1) < eps(100.0)
+    for s1 in (-1, +1), s2 in (-1, +1)
+        @test [s1, s2] in coeffs
+    end
+end
 
 println("\nfinished-----------------------------------------------------------")
