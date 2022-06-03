1bcc14ee26782e183bf46f13bf710e248f4335d7
guberger <guillaume.berger@colorado.edu>
2022-06-03 08:22:25 -0600

    Add example Polanski (3x3)

---
+++ examples/AdaptiveComplexity/exa_polanski/datasets/dataset_1.jl
@@ -0,0 +1,6 @@
+ϵ = 50.0
+θ = 1/16
+δ = 0.001
+α = 0.5
+
+# Find a polyhedral Lyapunov function with 369 pieces
---
+++ examples/AdaptiveComplexity/exa_polanski/main_compute.jl
@@ -0,0 +1,58 @@
+module ExampleZelentsowsky_Compute
+
+using LinearAlgebra
+using JuMP
+using Gurobi
+using PyPlot
+
+include("../../../src/CEGISPolyhedralLyapunov.jl")
+CPLA = CEGISPolyhedralLyapunov.AdaptiveComplexity
+CPLP = CEGISPolyhedralLyapunov.Polyhedra
+include("../../utils/geometry.jl")
+
+const GUROBI_ENV = Gurobi.Env()
+solver = optimizer_with_attributes(
+    () -> Gurobi.Optimizer(GUROBI_ENV), "OutputFlag"=>false
+)
+
+datafile = "dataset_1"
+include(string("./datasets/", datafile, ".jl"))
+
+## Parameters
+nvar = 3
+
+sys = CPLA.System()
+
+domain = CPLP.Cone()
+A = [-10.0 -2.0 -2.0; 1.0 0.0 0.0; 0.0 1.0 0.0]
+CPLA.add_piece!(sys, domain, A)
+
+domain = CPLP.Cone()
+A = [-10.0 -10.0*α -10.0*α; 1.0 0.0 0.0; 0.0 1.0 0.0]
+CPLA.add_piece!(sys, domain, A)
+
+## Learner feasible illustration
+lear = CPLA.Learner(nvar, sys, ϵ, θ, δ)
+CPLA.set_tol!(lear, :rad, 1e-6)
+
+points_init = [
+    [-1.0, 0.0, 0.0], [1.0, 0.0, 0.0],
+    [0.0, -1.0, 0.0], [0.0, 1.0, 0.0],
+    [0.0, 0.0, -1.0], [0.0, 0.0, 1.0]
+]
+for point in points_init
+    CPLA.add_point_init!(lear, point)
+end
+
+## Solving
+sol = CPLA.learn_lyapunov!(lear, 1000, solver)
+
+display(sol.status)
+
+f = open(string(@__DIR__, "/results/", datafile, ".txt"), "w")
+for vec in sol.vecs_list[sol.niter]
+    println(f, vec)
+end
+close(f)
+
+end # module
